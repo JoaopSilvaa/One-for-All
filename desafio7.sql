11e5024c38dfe0c5ffa6f02227a5d7f2056be299
Joao Antonio <joaopsilva.dev@gmail.com>
2022-06-03 16:09:28 -0300

    :heavy_check_mark: Requisito 7 completo, último envio para avaliação final com todos requisitos completos

--- desafio7.sql
+++ desafio7.sql
@@ -1,20 +1,11 @@
-SELECT
-	art.name AS artista,
-    alb.name AS album,
-	COUNT(usflart.user_id) AS seguidores
-FROM SpotifyClone.Artists art
-INNER JOIN SpotifyClone.Albums alb
-ON art.id = alb.artist_id
-INNER JOIN SpotifyClone.User_follow_artists usflart
-ON art.id = usflart.artist_id
-GROUP BY seguidores
-ORDER BY seguidores DESC, artista, album;
-
 SELECT 
-	art.name,
+	art.name artista,
+    alb.name album,
     COUNT(usflart.user_id) seguidores
 FROM SpotifyClone.Artists art
 INNER JOIN SpotifyClone.User_follow_artists usflart
 ON art.id = usflart.artist_id
-GROUP BY art.name
-ORDER BY seguidores DESC;
+LEFT JOIN SpotifyClone.Albums alb
+ON art.id = alb.artist_id
+GROUP BY art.name, alb.name
+ORDER BY seguidores DESC, art.name, alb.name;
