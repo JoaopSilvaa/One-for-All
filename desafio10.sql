e78683480a083315a7f44ab96a92b71d70f7bd2c
Joao Antonio <joaopsilva.dev@gmail.com>
2022-06-03 15:04:10 -0300

    :construction: Requisito 10 completo

--- desafio10.sql
+++ desafio10.sql
@@ -1,20 +1,17 @@
-SELECT
+SELECT DISTINCT
 	m.name nome,
     COUNT(his.music_id) reproducoes
-FROM SpotifyClone.Musics m
-INNER JOIN SpotifyClone.Historic his
-ON m.id = his.music_id
-GROUP BY m.name
-ORDER BY m.name;
-
-SELECT DISTINCT
-	us.name,
-    b.description
 FROM SpotifyClone.Users us
 INNER JOIN SpotifyClone.Bundles b
 ON us.bundle_id = b.id
+INNER JOIN SpotifyClone.Historic his
+ON us.id = his.user_id
+INNER JOIN SpotifyClone.Musics m
+ON m.id = his.music_id
 WHERE
 	b.description = 'gratuito'
     OR
-    b.description = 'pessoal';
+    b.description = 'pessoal'
+GROUP BY m.name
+ORDER BY m.name;
 
